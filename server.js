a7f09ed8bfadaa464a2f05bdfb1d594e1c2a0cf6
Hasnaa Al-Habahbeh <hasnaahab98@gmail.com>
2021-09-14 22:37:41 +0300

    final touches

--- server.js
+++ server.js
@@ -21,14 +21,14 @@ class ForCast {
 
 
 class Movie {
-    constructor(title, overview, vote_average, vote_count, image_url, popularity, release_date) {
+    constructor(title, overview, vote_average, vote_count, image_url, popularity, released_on) {
         this.title = title;
         this.overview = overview;
         this.average_votes = vote_average;
         this.total_votes = vote_count;
         this.image_url = image_url;
         this.popularity = popularity;
-        this.release_on = release_date;
+        this.released_on = released_on;
     }
 }
 
